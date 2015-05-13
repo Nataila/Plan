6f061009593ed31a19e25c57e92cd4cb57235b05
Nataila <ccfive814@gmail.com>
2015-05-13 09:30:23 +0800

    settings添加控制台日志输出

--- ui/settings.py
+++ ui/settings.py
@@ -82,3 +82,50 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+                'format': '%(levelname)s %(asctime)s %(message)s'
+                },
+    },
+    'filters': {
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'standard',
+        },
+        'test1_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'path1',
+            'formatter': 'standard',
+        },
+        'test2_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'path2',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'test1': {
+            'handlers': ['test1_handler'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'test2': {
+           'handlers': ['test2_handler'],
+           'level': 'INFO',
+           'propagate': False
+        },
+    }
+}

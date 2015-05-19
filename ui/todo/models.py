2e7eeddc0904c1babea09e9fbff8fc092c8a3fd4
Nataila <ccfive814@gmail.com>
2015-05-19 11:48:19 +0800

    添加django 的 TODO 数据模型

--- ui/todo/models.py
+++ ui/todo/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Nataila @ 2015-05-08
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Model):
+    """ TODO LIST """
+    TODO_TYPE = (
+        ('day', 'day'),
+        ('week', 'week'),
+        ('month', 'month')
+    )
+
+    user = models.OneToOneField(User, unique=True)
+    content = models.CharField(max_length=500, unique=True)
+    status = models.IntegerField()  # 完成状态，完成为1，未完成为0
+    created_at = models.TimeField(auto_now=True)
+    type = models.CharField(max_length=20, choices=TODO_TYPE, default='day')
+
+    class Meta:
+        db_table = "Todo"
+        ordering = ['created_at']

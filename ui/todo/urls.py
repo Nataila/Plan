d45514fceb7bbb0f4658baf6079981e87a07b5ee
Nataila <ccfive814@gmail.com>
2015-05-29 14:45:04 +0800

    更改目录结构

--- ui/todo/urls.py
+++ ui/todo/urls.py
@@ -5,10 +5,10 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'todo.views.index', {'template': 'todo/index.html'}),
-    url(r'^plan.html$', 'todo.views.plan', {'template': 'todo/plan.html'}),
-    url(r'^report$', 'todo.views.report', {'template': 'todo/report.html'}, 'report'),
-    url(r'^plan/detail$', 'todo.views.detail', {'template': 'todo/plan-detail.html'}),
+    url(r'^$', 'todo.views.index', {'template': 'todo/plan/index.html'}),
+    url(r'^plan.html$', 'todo.views.plan', {'template': 'todo/plan/plan.html'}),
+    url(r'^report$', 'todo.views.report', {'template': 'todo/plan/report.html'}, 'report'),
+    url(r'^plan/detail$', 'todo.views.detail', {'template': 'todo/plan/plan-detail.html'}),
     url(r'^save$', 'todo.views.save'),
     url(r'^get_default_data$', 'todo.views.get_default_data'),
     url(r'^change_status$', 'todo.views.change_status'),
